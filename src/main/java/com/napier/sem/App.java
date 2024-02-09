ec3a785355ff00fed918fe8c20a170b4ef83a06a
callum-sim <40340906@live.napier.ac.uk>
2024-02-09 19:26:49 +0000

    Committing first Java app

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,14 @@
+package com.napier.sem;
+
+public class App
+{
+
+    /**
+     * This is comment text
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
