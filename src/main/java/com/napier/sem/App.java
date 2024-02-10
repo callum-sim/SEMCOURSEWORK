98e4ed664b30794d32eb71275ad9762e57249ab5
callum-sim <40340906@live.napier.ac.uk>
2024-02-10 16:30:47 +0000

    Testing MongoDB server

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,13 +1,31 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+/**Mongo DB test below*/
 public class App
 {
-
-    /**
-     * This is comment text
-     */
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        // Connect to MongoDB on local system - we're using port 27000
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Get a database - will create when we use it
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        // Get a collection from the database
+        MongoCollection<Document> collection = database.getCollection("test");
+        // Create a document to store
+        Document doc = new Document("name", "Callum Sim")
+                .append("class", "Software Engineering Methods")
+                .append("year", "2024")
+                .append("result", new Document("CW", 95).append("EX", 85));
+        // Add document to collection
+        collection.insertOne(doc);
+
+        // Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
