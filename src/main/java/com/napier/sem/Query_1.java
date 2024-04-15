37033cdae6433e2eeef1de7bf22fb6ca63f130ac
chris <40675837@live.napier.ac.uk>
2024-04-15 21:10:29 +0100

    query 1 now working

--- src/main/java/com/napier/sem/Query_1.java
+++ src/main/java/com/napier/sem/Query_1.java
@@ -1,89 +1,76 @@
-//  Query 1: All the countries in the world organised by largest population to smallest
 package com.napier.sem;
 
 import java.sql.*;
 
-public class Query_1
-{
-    public static void main(String[] args)
-    {
-        App a = new App();
-        System.out.println("going in to connect");
-        if (args.length < 1) {
-            a.connect("localhost:33060", 10000);
+public class Query_1 {
+    static final int MAX_RETRIES = 10;
+
+    public static void main(String[] args) {
+        String location;
+        int delay;
+
+        if (args.length < 2) {
+            location = "localhost:33060";
+            delay = 10000;
         } else {
-            a.connect(args[0], Integer.parseInt(args[1]));
+            location = args[0];
+            delay = Integer.parseInt(args[1]);
         }
 
-
-
+        Query_1 query1 = new Query_1();
+        query1.connect(location, delay);
     }
-    /**
-     * Fetching all countries in the World largest to smallest
-     */
-    static Connection con = null;
+
     public void connect(String location, int delay) {
         try {
-            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
-            System.exit(-1);
+            e.printStackTrace();
+            return;
         }
 
-        int retries = 10;
-        boolean shouldWait = false;
-        for (int i = 0; i < retries; ++i) {
-            System.out.println("Connecting to database...");
-            try {
+        Connection connection = null;
+        try {
+            int retries = 0;
+            boolean shouldWait = false;
+            while (retries < MAX_RETRIES) {
                 if (shouldWait) {
-                    // Wait a bit for db to start
                     Thread.sleep(delay);
                 }
+
                 try {
-                    // Establishing a connection to the database
-                    Connection connection = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                    connection = DriverManager.getConnection("jdbc:mysql://" + location + "/world?useSSL=false", "root", "example");
 
-                    // Creating and executing the SQL query
                     String query = "SELECT * FROM country ORDER BY population DESC";
-                    Statement statement = connection.createStatement();
-                    ResultSet resultSet = statement.executeQuery(query);
-
-                    // Processing the results
-                    while(resultSet.next()) {
-                        // Accessing and printing data for each country
-                        String countryName = resultSet.getString("name");
-                        int population = resultSet.getInt("population");
-                        System.out.println("Country: " + countryName + ", Population: " + population);
+                    try (Statement statement = connection.createStatement();
+                         ResultSet resultSet = statement.executeQuery(query)) {
+                        while (resultSet.next()) {
+                            String countryName = resultSet.getString("name");
+                            int population = resultSet.getInt("population");
+                            System.out.println("Country: " + countryName + ", Population: " + population);
+                        }
                     }
+                    break; // Successful retrieval, break out of retry loop
+                } catch (SQLException e) {
+                    System.out.println("Failed to connect to database attempt " + (retries + 1));
+                    System.out.println(e.getMessage());
+                    shouldWait = true;
+                }
 
-                    // Closing the resources
-                    resultSet.close();
-                    statement.close();
+                retries++;
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted? Should not happen.");
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
                     connection.close();
-
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-                // Connect to database
-                System.out.println("Going in to  connect");
-                con = DriverManager.getConnection("jdbc:mysql://" + location
-                                + "/world?allowPublicKeyRetrieval=true&useSSL=false",
-                        "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + i);
-                System.out.println(sqle.getMessage());
-
-                // Let's wait before attempting to reconnect
-                shouldWait = true;
-            } catch (InterruptedException ie) {
-                System.out.println("Thread interrupted? Should not happen.");
             }
         }
-
     }
-
-
 }
