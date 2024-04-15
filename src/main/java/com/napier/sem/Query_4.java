f47777a391e30bd8f777c8bad6fb171f1a2e043d
40340906 <40340906@live.napier.ac.uk>
2024-04-15 23:36:11 +0100

    Query 4 comments added

--- src/main/java/com/napier/sem/Query_4.java
+++ src/main/java/com/napier/sem/Query_4.java
@@ -2,6 +2,7 @@ package com.napier.sem;
 
 import java.sql.*;
 
+
 /**
  * The below code queries the database to return countries, ordering populations
  * from largest to smallest, limited to the top 10
